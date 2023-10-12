67472ddc5f1c41b8d90ffd7862523c36c871eda2
Julen Ercibengoa PC <julenertzisa@gmail.com>
2023-10-12 16:58:21 +0200

    Environment change: create execute_environment

--- classes.py
+++ classes.py
@@ -6,21 +6,25 @@ import os
 from helper import move
 import numpy as np
 
-class rabbit:
-  def __init__(self,x_min,x_max,y_min,y_max,v_min,v_max,r_min,r_max):
+class Rabbit:
+  def __init__(self,x_min,x_max,y_min,y_max,v_min,v_max,r_min,r_max,start_iteration):
     self.x_min = x_min
     self.x_max = x_max
     self.y_min = y_min
     self.y_max = y_max
+    self.start_iteration = start_iteration # For child rabbits
+    self.age = 1
     
-    self.initial_x = randint(x_min,x_max)
-    self.initial_y = randint(y_min,y_max)
+    if self.start_iteration == 1:
+      self.initial_x = randint(x_min,x_max)
+      self.initial_y = randint(y_min,y_max)
+
     self.positions = np.array([[self.initial_x,self.initial_y]])
     self.max_speed = randint(r_min,r_max)
     self.speed = randint(v_min,v_max)
     self.age = 1 # age goes from 1 to 10
 
-  def move_bunny(self):
+  def move_rabbit(self):
     """
     Takes the last position (x,y) from the array self.positions and based on self.max_speed, 
     adds a new position to the self.positions array
@@ -40,10 +44,36 @@ class Fox:
     pass
     
 class Environment:
-  def __init__(self):
-    pass
+  def __init__(self,n_iterations,n_rabbits,n_foxes,x_min,x_max,y_min,y_max,v_min,v_max,r_min,r_max):
+    self.n_iterations = n_iterations # Initial position will be counted as iteration 0, therefore, in total n_iterations + 1 positions for each rabbit/fox
+    self.n_rabbits = n_rabbits
+    self.n_foxes = n_foxes
+    self.x_min = x_min
+    self.x_max = x_max
+    self.y_min = y_min
+    self.y_max = y_max
+    self.v_min = v_min
+    self.v_max = v_max
+    self.r_min = r_min
+    self.r_max = r_max
+
+    # Start initial rabbits:
+    self.rabbits = []
+    for i in range(self.n_rabbits):
+      self.rabbits.append(Rabbit(self.x_min,self.x_max,self.y_min,self.y_max,self.v_min,self.v_max,self.r_min,self.r_max,1))
+    
+  def execute_environment(self):
+    """
+    Executes the simulation
+    """
+    for _ in range(self.n_iterations):
+      for rabbit in self.rabbits:
+        rabbit.move_rabbit()
+    for i in range(len(self.rabbits)):
+      print(f"Rabbit {i}  position: {self.rabbits[i].positions}")
 
   def animate_environment(self):
+    
     pass
 
   def distances():
